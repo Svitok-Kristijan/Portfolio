f167405d77df162ab36beb9bf9dd97cefe39de52
Svitok-Kristijan <92441179+Svitok-Kristijan@users.noreply.github.com>
2023-06-11 14:17:06 +0200

    update

--- src/monster/monster.jsx
+++ src/monster/monster.jsx
@@ -5,10 +5,7 @@ import "./monster.scss";
 const Monster = () => {
   return (
     <div className="projects-holderMonster">
-      <p>
-        This is my banking website created with the help of paid courses. Only
-        HTML, CSS, and JS were used.
-      </p>
+      <p>This is one of the first React pages I created as an exercise.</p>
       <a
         className="git-forky"
         href="https://github.com/Svitok-Kristijan/Monster"
